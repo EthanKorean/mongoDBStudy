6fa81489ad62d32af06948efff417c15499c6b34
kdhgl <kdhgla@gmail.com>
2021-11-29 23:08:44 +0900

    게시판 생성으로 변경

--- batch/mongo_create.bat
+++ batch/mongo_create.bat
@@ -1,9 +1,9 @@
 cd C:\Program Files\MongoDB\Server\5.0\bin
 
 mongo.exe mongo_study --eval "db.user.drop()"
-mongo.exe mongo_study --eval "db.group.drop()"
+mongo.exe mongo_study --eval "db.board.drop()"
 
 mongo.exe mongo_study --eval "db.user.insert({login_id:'admin',pwd:'admin',email:'',phone:'',mobile:'',status:'',create_dt:'',modify_dt:'',end_dt:''});"
-mongo.exe mongo_study --eval "db.group.insert({code:'admin',name:'기본부서',status:'',create_dt:'',modify_dt:'',end_dt:''});"
+mongo.exe mongo_study --eval "db.board.insert({title:'',content:'',writer:'',create_dt:'',modify_dt:'',end_dt:''});"
 
 pause
